95fff1e73cbb694eded2a04c682ca7564b0c9d17
biyamn <b1y9mn@gmail.com>
2022-10-31 21:27:50 +0900

    Chore: tailwind.config.js에 extend 추가

--- src/pages/Todo/Todo.js
+++ src/pages/Todo/Todo.js
@@ -1,8 +1,61 @@
+import React from 'react';
+
 const Todo = () => {
   return (
-    <main>
-      <section>todo가 들어갈거에요</section>
-    </main>
+    <div>
+      <div className="big-container">
+        <div className="small-container">
+          <div className="title">
+            <h1>오늘의 할 일</h1>
+            <div className="filter">
+              <button className="filter__all">전체</button>/
+              <button className="filter__done">완료</button>/
+              <button className="filter__yet">미완료</button>
+            </div>
+          </div>
+          <div className="smallContainer">
+            <form>
+              <div className="input">
+                <input 
+                  className='input__text' 
+                  type='text' 
+                  placeholder='일감 입력하기'
+                />
+                <input 
+                  className='input__submit' 
+                  type='submit' 
+                  value='제출'
+                />
+                </div>
+              <div className="lists">
+                <div className="list">
+                  <input
+                    className="list__checkbox" 
+                    type='checkbox'
+                  />
+                  <span className="list__content">강아지 산책</span>
+                  <button 
+                    type='button'
+                    className="list__edit-btn" 
+                  >수정</button>
+                  <button 
+                    type='button' 
+                    className='list__delete-btn'
+                  >삭제</button>
+                </div>
+              </div>
+            </form>
+            </div>
+        </div>
+        <div className="share">
+          <input
+            className="shareBtn"
+            type="submit"
+            value="SNS 공유"
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,34 +2,36 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "976px",
-      xl: "1440px",
-    },
-    colors: {
-      primary: "#4B6BFB",
-      secondary: "#7B92B2",
-      accent: "#67CBA0",
-      neutral: "#181A2A",
-      "base-100": "#FFFFFF",
-      info: "#3ABFF8",
-      success: "#36D399",
-      warning: "#FBBD23",
-      error: "#F87272",
-    },
-    fontFamily: {
-      sans: ["Graphik", "sans-serif"],
-      serif: ["Merriweather", "serif"],
-    },
     extend: {
-      spacing: {
-        128: "32rem",
-        144: "36rem",
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
       },
-      borderRadius: {
-        "4xl": "2rem",
+      colors: {
+        primary: "#4B6BFB",
+        secondary: "#7B92B2",
+        accent: "#67CBA0",
+        neutral: "#181A2A",
+        "base-100": "#FFFFFF",
+        info: "#3ABFF8",
+        success: "#36D399",
+        warning: "#FBBD23",
+        error: "#F87272",
+      },
+      fontFamily: {
+        sans: ["Graphik", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+      extend: {
+        spacing: {
+          128: "32rem",
+          144: "36rem",
+        },
+        borderRadius: {
+          "4xl": "2rem",
+        },
       },
     },
   },
